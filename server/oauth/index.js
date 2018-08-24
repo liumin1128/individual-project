c74e09e66780b1b02e8083364a7e12fc6bf5a834
liumin1128 <970568830@qq.com>
2018-08-24 19:11:46 +0800

    更新

--- server/oauth/index.js
+++ server/oauth/index.js
@@ -1,12 +1,9 @@
 import Router from 'koa-router';
-import github from './github';
 import outlook from './outlook';
 
 const router = new Router();
 
 export default router
-  .get('/github', github.login)
-  .get('/github/callback', github.callback)
   .get('/outlook', outlook.login)
   .get('/outlook/callback', outlook.callback)
   .get('/outlook/test', outlook.test);
